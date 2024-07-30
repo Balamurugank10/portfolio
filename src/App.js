9b46ccdb3c5b3d400e25583d83b3c5c316e27ca9
Balamurugank10 <a20cada56@sjctnc.edu.in>
2024-07-30 11:13:01 +0530

    simple navigation added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {Routes, Route} from 'react-router-dom';
+import Home from './container/home';
+import About from './container/about';
+import Resume from './container/resume';
+import Projects from './container/projects';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route index path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/resume' element={<Resume/>} />
+        <Route path='projects' element={<Projects/>}/>
+      </Routes>
     </div>
   );
 }
